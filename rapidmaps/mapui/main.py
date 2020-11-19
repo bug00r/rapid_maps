cd73180b67c215495e7de7f8655ea967190af322
bug00r <bachus@gmx.net>
2020-11-19 17:12:17 +0100

    add first simple drawings

--- rapidmaps/mapui/main.py
+++ rapidmaps/mapui/main.py
@@ -1,7 +1,96 @@
 from rapidmaps.mapui.wxui.generated.rapidmap import MainFrame
+from typing import Any
+from wx import Point as wxPoint
+from wx import AutoBufferedPaintDC as abDC
+from wx import Size, BG_STYLE_PAINT, Brush, Colour, BG_STYLE_COLOUR
+
+class Shape(object):
+
+    def __init__(self):
+        self._pos = wxPoint(0, 0)
+
+    def set_position(self, position: wxPoint):
+        self._pos = position
+
+    def draw_by_dc(self, dc: Any):
+        pass
+
+
+class Point(Shape):
+    def __init__(self):
+        super().__init__()
+
+    def draw_by_dc(self, dc: Any):
+        dc.DrawPoint(self._pos)
+        print(f"Point: {self._pos}")
+
+
+class Circle(Shape):
+    def __init__(self):
+        super().__init__()
+
+    def draw_by_dc(self, dc: Any):
+        dc.DrawCircle(self._pos, 5)
+        print(f"Circle: {self._pos}")
+
+
+class Quad(Shape):
+    def __init__(self):
+        super().__init__()
+
+    def draw_by_dc(self, dc: Any):
+        dc.DrawRectangle(pt=self._pos, sz=Size(20, 20))
+        print(f"Quad: {self._pos}")
+
+
+class Triangle(Shape):
+    def __init__(self):
+        super().__init__()
+
+    def draw_by_dc(self, dc: Any):
+        print(f"Triangle: {self._pos}")
 
 
 class RapidMapFrame(MainFrame):
 
     def __init__(self):
         super().__init__(None)
+        self.canvas.SetBackgroundStyle(BG_STYLE_PAINT)
+        self.__shape_clz = [Point, Quad, Circle, Triangle]
+        self.__shape_obj = []
+        self.__sel_action = 0
+        self.__lm_release = None
+        self.__sel_shape = None
+
+    def OnActionChange(self, event):
+        #event.Skip()
+        self.__sel_action = event.Selection
+        self.m_shapes.Enable(enable=self.should_add_entity())
+
+    def OnShapeChange(self, event):
+        event.Skip()
+
+    def OnMouseLeftDown(self, event):
+        event.Skip()
+
+    def OnMouseLeftUp(self, event):
+        if self.should_add_entity():
+            self.__lm_release = event.Position
+            self.__sel_shape = self.m_shapes.Selection
+            new_obj = self.__shape_clz[self.__sel_shape]()
+            new_obj.set_position(position=self.__lm_release )
+            self.__shape_obj.append(new_obj)
+            print(f"x: {self.__lm_release[0]} y: {self.__lm_release[1]} shape: {self.__sel_shape}")
+            self.canvas.Refresh()
+
+    def OnMouseMotion(self, event):
+        event.Skip()
+
+    def OnPaint(self, event):
+        dc = abDC(self.canvas)
+        dc.SetBackground(Brush(Colour(255, 255, 255)))
+        for shape in self.__shape_obj:
+            shape.draw_by_dc(dc)
+
+    def should_add_entity(self):
+        return self.__sel_action == 1
--- rapidmaps/mapui/wxui/generated/rapidmap.py
+++ rapidmaps/mapui/wxui/generated/rapidmap.py
@@ -20,6 +20,7 @@ class MainFrame ( wx.Frame ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Rapid Mapping tool", pos = wx.DefaultPosition, size = wx.Size( 1208,728 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 
 		self.m_statusmain = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
 		self.m_menubar1 = wx.MenuBar( 0 )
@@ -41,17 +42,17 @@ class MainFrame ( wx.Frame ):
 		self.m_panel3 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		bSizer31 = wx.BoxSizer( wx.VERTICAL )
 
-		m_radioBox21Choices = [ u"Select", u"Add" ]
-		self.m_radioBox21 = wx.RadioBox( self.m_panel3, wx.ID_ANY, u"Actions", wx.DefaultPosition, wx.DefaultSize, m_radioBox21Choices, 1, wx.RA_SPECIFY_COLS )
-		self.m_radioBox21.SetSelection( 0 )
-		bSizer31.Add( self.m_radioBox21, 0, wx.ALL|wx.EXPAND, 5 )
+		m_actionsChoices = [ u"Select", u"Add" ]
+		self.m_actions = wx.RadioBox( self.m_panel3, wx.ID_ANY, u"Actions", wx.DefaultPosition, wx.DefaultSize, m_actionsChoices, 1, wx.RA_SPECIFY_COLS )
+		self.m_actions.SetSelection( 0 )
+		bSizer31.Add( self.m_actions, 0, wx.ALL|wx.EXPAND, 5 )
 
-		m_radioBox2Choices = [ u"Point", u"Quad", u"Circle", u"Triangle" ]
-		self.m_radioBox2 = wx.RadioBox( self.m_panel3, wx.ID_ANY, u"Shapes", wx.DefaultPosition, wx.DefaultSize, m_radioBox2Choices, 1, wx.RA_SPECIFY_COLS )
-		self.m_radioBox2.SetSelection( 0 )
-		self.m_radioBox2.Enable( False )
+		m_shapesChoices = [ u"Point", u"Quad", u"Circle", u"Triangle" ]
+		self.m_shapes = wx.RadioBox( self.m_panel3, wx.ID_ANY, u"Shapes", wx.DefaultPosition, wx.DefaultSize, m_shapesChoices, 1, wx.RA_SPECIFY_COLS )
+		self.m_shapes.SetSelection( 0 )
+		self.m_shapes.Enable( False )
 
-		bSizer31.Add( self.m_radioBox2, 0, wx.ALL, 5 )
+		bSizer31.Add( self.m_shapes, 0, wx.ALL, 5 )
 
 
 		self.m_panel3.SetSizer( bSizer31 )
@@ -64,6 +65,7 @@ class MainFrame ( wx.Frame ):
 		bSizer3 = wx.BoxSizer( wx.VERTICAL )
 
 		self.canvas = wx.Panel( self.m_scrolledWindow1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.canvas.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWTEXT ) )
 		self.canvas.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWTEXT ) )
 
 		bSizer3.Add( self.canvas, 1, wx.EXPAND |wx.ALL, 5 )
@@ -81,11 +83,12 @@ class MainFrame ( wx.Frame ):
 		self.Centre( wx.BOTH )
 
 		# Connect Events
-		self.m_radioBox21.Bind( wx.EVT_RADIOBOX, self.OnActionChange )
-		self.m_radioBox2.Bind( wx.EVT_RADIOBOX, self.OnShapeChange )
+		self.m_actions.Bind( wx.EVT_RADIOBOX, self.OnActionChange )
+		self.m_shapes.Bind( wx.EVT_RADIOBOX, self.OnShapeChange )
 		self.canvas.Bind( wx.EVT_LEFT_DOWN, self.OnMouseLeftDown )
 		self.canvas.Bind( wx.EVT_LEFT_UP, self.OnMouseLeftUp )
 		self.canvas.Bind( wx.EVT_MOTION, self.OnMouseMotion )
+		self.canvas.Bind( wx.EVT_PAINT, self.OnPaint )
 
 	def __del__( self ):
 		pass
@@ -105,6 +108,9 @@ class MainFrame ( wx.Frame ):
 		event.Skip()
 
 	def OnMouseMotion( self, event ):
+		event.Skip()
+
+	def OnPaint( self, event ):
 		event.Skip()
 
 
